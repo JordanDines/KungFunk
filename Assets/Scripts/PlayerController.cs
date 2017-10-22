169bd238c3347cc6ee5f5f1b3811b1f42052390f
JordanDines <jordan@dines.net.au>
2017-10-22 23:07:10 +1100

    Project Start.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 1;
+
+	
+	void Start () {
+		
+	}
+
+
+    void Update()
+    { 
+        for(int i = 0; 0 < Input.touchCount; ++i)
+
+            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Debug.Log("Yay");
+                // Construct a ray from the current touch coordinates
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                Debug.Log(ray + "help");
+                    // Create a particle if hit
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                    transform.position = Vector2.Lerp(transform.position, ray.direction, speed);
+                    }
+            }
+        
+    }
+}
