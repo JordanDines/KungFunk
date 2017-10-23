18cd69f90996c5cc23262ef7cac544c5075cd482
JordanDines <jordan@dines.net.au>
2017-10-23 22:27:52 +1100

    Fixed null reference in camera. Can't figure out where to go from there

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,29 +6,52 @@ public class PlayerController : MonoBehaviour {
 
     public int speed = 1;
 
-	
-	void Start () {
-		
+    private Camera cam;
+
+    // Use this for initialization
+    void Start () {
+        cam = FindObjectOfType<Camera>();
 	}
-
-
-    void Update()
-    { 
-        for(int i = 0; 0 < Input.touchCount; ++i)
+	
+	// Update is called once per frame
+	void Update () {
+        //for (var i = 0; i < Input.touchCount; ++i)
+        //{
+        //    if (Input.GetTouch(i).phase == TouchPhase.Began)
+        //    {
+        //        RaycastHit hit;
+        //        // Construct a ray from the current touch coordinates
+        //        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
+        //        Debug.Log("yeahhh");
+        //        transform.position = ray.origin.
+        //        // Create a particle if hit
+        //        if (Physics.Raycast(ray, out hit))
+        //        { }
+        //        //transform.position;
+        //    }
+        //}
+        for (int i = 0; i < Input.touchCount; ++i)
+        {
 
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
-                Debug.Log("Yay");
+               
                 // Construct a ray from the current touch coordinates
                 RaycastHit hit;
                 Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
                 Debug.Log(ray + "help");
-                    // Create a particle if hit
-                    if (Physics.Raycast(ray, out hit))
+                // Works up to here
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+                {
+                    if (hit.collider.tag == "Enemy")
                     {
-                    transform.position = Vector2.Lerp(transform.position, ray.direction, speed);
+                        Debug.Log("Yay");
+                        Vector2.Lerp(transform.position, hit.transform.position, speed);
                     }
+                    
+                }
             }
-        
+        }
+
     }
 }
