0bcca6fa663d871ba2f7a2001f85e908c686bd08
JordanDines <jordan@dines.net.au>
2017-10-30 21:30:56 +1100

    TOUCH CONTROLS WORK! Changed to 3D. No more sprites.
    -Jordan

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,54 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public int speed = 1;
+    public Color initialColour;
+    public Color selectedColour;
+    private Material mat;
 
-    private Camera cam;
+    void Start ()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
 
-    // Use this for initialization
-    void Start () {
-        cam = FindObjectOfType<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //for (var i = 0; i < Input.touchCount; ++i)
-        //{
-        //    if (Input.GetTouch(i).phase == TouchPhase.Began)
-        //    {
-        //        RaycastHit hit;
-        //        // Construct a ray from the current touch coordinates
-        //        Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
-        //        Debug.Log("yeahhh");
-        //        transform.position = ray.origin.
-        //        // Create a particle if hit
-        //        if (Physics.Raycast(ray, out hit))
-        //        { }
-        //        //transform.position;
-        //    }
-        //}
-        for (int i = 0; i < Input.touchCount; ++i)
-        {
-
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-            {
-               
-                // Construct a ray from the current touch coordinates
-                RaycastHit hit;
-                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-                Debug.Log(ray + "help");
-                // Works up to here
-                if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-                {
-                    if (hit.collider.tag == "Enemy")
-                    {
-                        Debug.Log("Yay");
-                        Vector2.Lerp(transform.position, hit.transform.position, speed);
-                    }
-                    
-                }
-            }
-        }
-
+    void OnTouchDown ()
+    {
+        mat.color = selectedColour;
+    }
+    void OnTouchUp()
+    {
+        mat.color = initialColour;
+    }
+    void OnTouchStay()
+    {
+        mat.color = selectedColour;
+    }
+    void OnTouchExit()
+    {
+        mat.color = initialColour;
     }
 }
