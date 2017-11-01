44d5bdb31d8ce28ede8573c7ff5a4940153e5189
JordanDines <jordan@dines.net.au>
2017-11-01 16:03:36 +1100

    Made Player Movement.
    Added Enemy script and Prefab
    
    -Jordan

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public Color initialColour;
+    public Color selectedColour;
+    public float speed;
+    private Material mat;
+
+    public GameObject player;
+
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    void OnTouchDown()
+    {
+        mat.color = selectedColour;
+        
+    }
+    void OnTouchUp()
+    {
+        mat.color = initialColour;
+    }
+    void OnTouchStay()
+    {
+        mat.color = selectedColour;
+        player.transform.position = Vector3.Lerp(player.transform.position, transform.position, speed);
+    }
+    void OnTouchExit()
+    {
+        mat.color = initialColour;
+    }
+}
