cac6881e3f040a8234adcfb519e9d9de12c08e09
JordanDines <jordan@dines.net.au>
2018-02-11 01:45:09 +1100

    Got movement to finally work
    -Jordan

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour {
     public Vector2 newPosition;
     public float angle;
 
+    public Vector3 firstPosition;
+    public Vector3 lastPostiion;
 
     private Camera cam;
 
@@ -42,24 +44,43 @@ public class PlayerController : MonoBehaviour {
 
     void GetSwipeDirection()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
-            oldPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            firstPosition = transform.position;
+
         }
-        //When pressing down, get the first position
+
         if (Input.GetMouseButton(0))
         {
             movingPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+            
         }
-        //When letting go, get the final position
         if (Input.GetMouseButtonUp(0))
         {
-            newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
-            
             
         }
+
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    oldPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //}
+        //When pressing down, get the first position
+        //if (Input.GetMouseButton(0))
+        //{
+        //    movingPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        //}
+        //When letting go, get the final position
+        //if (Input.GetMouseButtonUp(0))
+        //{
+        //    newPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+
+
+        //}
         //Get the angle between the two position
-        angle = Vector2.Angle(oldPosition, newPosition);
+        //angle = Vector2.Angle(oldPosition, newPosition);
 
         //Loop through each enemy, check the angle between their position and your position
         //against swipe angle. return the ones that that are within a threshold using MathfDeltaAngle(eulerAngle1, eulerAngle2)
@@ -67,13 +88,13 @@ public class PlayerController : MonoBehaviour {
         //if two are on the same angle, get the closest one using vector3.distance
 
         //If the angle goes beyond 180, make the angle minus from 360
-        Vector3 cross = Vector3.Cross(newPosition, oldPosition);
-        if (cross.z < 0)
-        {
-            angle = 360 - angle;
-        }
+        //Vector3 cross = Vector3.Cross(newPosition, oldPosition);
+        //if (cross.z < 0)
+        //{
+        //    angle = 360 - angle;
+        //}
 
-
+        Debug.DrawLine(firstPosition, movingPosition);
         Debug.DrawLine(oldPosition, movingPosition);
 
 
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 10f;
+
+    public Vector3 startPosition;
+    public Vector3 letGoPosition;
+	public GameObject endPosition;
+	public Vector3 draggingPosition;
+
+	public GameObject enemyToMoveTo;
+	public bool isMoving = false;
+	public float minDistance = 1;
+
+    public Camera cam;
+	public GameObject[] enemies;
+
+
+	public GameObject[] triggerList;
+	// Use this for initialization
+	void Start () {
+		
+        cam = FindObjectOfType<Camera>();
+		GetComponent<CapsuleCollider> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetMouseButtonDown(0))
+        {
+			GetComponent<CapsuleCollider> ().enabled = true;
+			startPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+			Debug.DrawLine (startPosition, draggingPosition);
+        }
+
+		if (Input.GetMouseButton (0)) {
+			draggingPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+			Debug.DrawLine (startPosition, draggingPosition, Color.blue);
+		}
+
+		if (Input.GetMouseButtonUp (0)) {
+			draggingPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+
+			//Get the angle between the points
+			float angle = AngleBetweenTwoPoints(startPosition, draggingPosition);
+
+			transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,(angle + 90f)));
+			Invoke ("TurnOffMovingTrigger", .2f);
+		}
+		if (isMoving) {
+			MoveToEnemy();
+		}
+
+	}
+		
+
+	//called when something enters the trigger
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Enemy") {
+			enemyToMoveTo = other.gameObject;
+			print (other.gameObject.tag);
+			isMoving = true;
+		}
+
+		//enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+		//GameObject closestEnemy = null;
+		//float distanceToClosestEnemy = Mathf.Infinity;
+		//foreach (GameObject enemy in enemies) 
+		//{
+		//	float distanceToEnemy = Vector3.Distance(endPosition.transform.position, enemy.transform.position);
+		//	if (distanceToEnemy < distanceToClosestEnemy) 
+		//	{
+		//		closestEnemy = enemy;
+		//		distanceToClosestEnemy = distanceToEnemy;
+		//	}
+		//}
+		//transform.position = closestEnemy.transform.position;
+		//print (closestEnemy);
+	}
+		
+
+	void TurnOffMovingTrigger(){
+		GetComponent<CapsuleCollider> ().enabled = false;
+	}
+
+	void MoveToEnemy (){
+		transform.position = Vector3.Lerp (transform.position, enemyToMoveTo.transform.position, speed);
+		if (Vector3.Distance (transform.position, enemyToMoveTo.transform.position) < minDistance) {
+			isMoving = false;
+		}
+	}
+		
+	float AngleBetweenTwoPoints(Vector3 a, Vector3 b) {
+	
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
+
+	//void GetClosestEnemy(){
+	//	GameObject closestEnemy = null;
+	//	float distanceToClosestEnemy = Mathf.Infinity;
+	//	foreach (GameObject enemy in enemies) 
+	//	{
+	//		float distanceToEnemy = Vector3.Distance(endPosition.transform.position, enemy.transform.position);
+	//		if (distanceToEnemy < distanceToClosestEnemy) 
+	//		{
+	//			closestEnemy = enemy;
+	//			distanceToClosestEnemy = distanceToEnemy;
+	//		}
+	//	}
+	//	print (closestEnemy);
+	//	transform.position = closestEnemy.transform.position;
+	//}
+}
